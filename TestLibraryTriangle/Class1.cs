961d1aab41c0572f56685621c79af4997d82c56f
Dizza Harkhani <dharkhani2375@conestogac.on.ca>
2025-02-09 19:51:25 -0500

    Pushed triangle app project

---
+++ TestLibraryTriangle/Class1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using ConsoleTriangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibraryTriangle
+{
+   
+        [TestFixture]
+        public class Class1
+        {
+            [Test]
+            public void ValidTriangle_Input60and60_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+
+                //Act
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                Assert.That(expected, Is.EqualTo(actual));
+            }
+
+        }
+    }
+
+
+
